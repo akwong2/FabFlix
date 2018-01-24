7755dcc92bd27f16b788bb215038c68ff3dcc6f4
Alvin <akwong2@uci.edu>
2018-01-23 16:56:14 -0800

    project1

--- project1/src/MovieList.java
+++ project1/src/MovieList.java
@@ -1,11 +1,20 @@
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.annotation.WebServlet;
+
+import java.sql.*;
 
 /**
  * Servlet implementation class MovieList
@@ -28,6 +37,83 @@ public class MovieList extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		
+		String loginUser = "mytestuser";
+        String loginPasswd = "mypassword";
+        String loginUrl = "jdbc:mysql://moviedb?autoReconnect=true&useSSL=false";
+        
+        response.setContentType("text/html");
+        
+        out.println("<HTML><HEAD><TITLE>MovieDB</TITLE></HEAD>");
+        out.println("<BODY><H1>Top 20 Rated Movies2</H1>");
+ 
+        try {
+        		out.println("<H3>ONE</H3>");
+	        Class.forName("com.mysql.jdbc.Driver").newInstance();
+	        out.println("<H2>TWO</H2>");
+
+	        Connection dbcon = DriverManager.getConnection(loginUrl,loginUser, loginPasswd);
+	
+			Statement statement = dbcon.createStatement();
+			String query = "Select title,year,director,rating, group_concat(distinct stars.name), group_concat(distinct genres.name) "
+					+ "From movies, ratings, stars_in_movies, stars, genres, genres_in_movies "
+					+ "Where ratings.movieId = movies.id and stars_in_movies.movieId = movies.id "
+					+ "and stars_in_movies.starId = stars.id and genres.id = genres_in_movies.genreId and genres_in_movies.movieId = movies.id "
+					+ "group by movies.title,movies.year,movies.director,ratings.rating "
+					+ "order by rating desc "
+					+ "limit 20";
+			ResultSet rs = statement.executeQuery(query);
+			
+			out.println("<TABLE border>");
+			out.println("<tr>"
+						+ "<th>Movie Number</th>"
+						+ "<th>Title</th>" 
+						+ "<th>Year</th>"
+						+ "<th>Director</th>"
+						+ "<th>List of Genres</th>"
+						+ "<th>List of Stars</th>"
+						+ "<th>Rating</th>"
+					+ "</tr>");
+			int counter = 1;
+			while (rs.next()) {
+				String m_title = rs.getString("title");
+				String m_year = rs.getString("year");
+				String m_director = rs.getString("director");
+				String m_lgenres = rs.getString("group_concat(distinct genres.name)");
+				String m_lstars = rs.getString("group_concat(distinct stars.name)");
+				String m_rating = rs.getString("rating");
+	            	out.println("<tr>"
+	            				+ "<td>"+counter+"</td>"
+            					+ "<td>"+m_title+"</td> "
+            					+ "<td>"+m_year +"</td>"
+            					+ "<td>"+m_director+"</td>"
+            					+ "<td>"+m_lgenres+"</td>"
+            					+ "<td>"+m_lstars+"</td>"
+            					+ "<td>"+m_rating+"</td>"
+        					+ "</tr>");
+	            	counter++;
+	            
+			}
+			out.println("</TABLE>");
+			rs.close();
+	        statement.close();
+	        dbcon.close();
+        }
+        catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println("SQL Exception:  " + ex.getMessage());
+                ex = ex.getNextException();
+            } // end while
+        } // end catch SQLException
+
+        catch (java.lang.Exception ex) {
+            out.println("<HTML>" + "<HEAD><TITLE>" + "MovieDB: Error" + "</TITLE></HEAD>\n<BODY>"
+                    + "<P>SQL error in doGet: " + ex.getMessage() + "</P></BODY></HTML>");
+            return;
+        }
+        out.close();
+		
 	}
 
 	/**
@@ -37,5 +123,4 @@ public class MovieList extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
